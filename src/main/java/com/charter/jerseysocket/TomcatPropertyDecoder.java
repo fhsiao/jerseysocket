4060129d31d0b1a213893574fe50c08ce042e370
fhsiao <frank.hsiao@hotmail.com>
2018-10-04 20:26:39 -0600

    VIDOPSTECH-8242: Add TomcatPropertyDecoder

---
+++ src/main/java/com/charter/jerseysocket/TomcatPropertyDecoder.java
@@ -0,0 +1,25 @@
+package com.charter.jerseysocket;
+
+import com.bettercloud.vault.VaultException;
+import com.charter.jerseysocket.vault.VaultClient;
+import org.apache.log4j.Logger;
+import org.apache.tomcat.util.IntrospectionUtils;
+
+
+public class TomcatPropertyDecoder implements IntrospectionUtils.PropertySource {
+    private static final Logger logger = Logger.getLogger(TomcatPropertyDecoder.class);
+
+    @Override
+    public String getProperty(String arg0) {
+        try {
+            String sVault = VaultClient.getVault();
+            logger.debug("getProperty: "+sVault);
+            return sVault;
+        } catch (VaultException e) {
+            logger.debug("getProperty() error: "+e.fillInStackTrace());
+            e.printStackTrace();
+        }
+        return "";
+    }
+
+}
